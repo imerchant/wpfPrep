e19fa238937e7b0a90267950333cd7658fc34a81
Imran Merchant <imerchant@outlook.com>
2017-08-03 08:27:55 -0500

    added style tests, added 'bad' image loading

---
+++ src/ShoppingCart/ImageFetcher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace ShoppingCart
+{
+	public class ImageFetcher : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value != null)
+			{
+				var uri = new Uri(value.ToString(), UriKind.Absolute);
+				var image = new BitmapImage(uri);
+				return image;
+			}
+
+			return null;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
